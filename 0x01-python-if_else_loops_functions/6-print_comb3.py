70072cd838c949c5afeb736dffe8e8f4b8dcb032
holic65 <okormark@gmail.com>
2022-06-01 16:30:16 -0700

    Task fixed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,7 +4,7 @@ while i < 89:
     if i % 10 == 0:
         i += 1 + i//10
     if i < 89:
-        print("{:0>2d}, " .format(i),end="")
+        print("{:0>2d}, " .format(i), end="")
     elif i == 89:
         print(89)
     i += 1
