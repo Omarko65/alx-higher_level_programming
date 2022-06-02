414542574b2ef50e6d48dcac91a9267723fcd925
holic65 <okormark@gmail.com>
2022-06-02 15:47:53 -0700

    Task 4 push

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import hidden_4
+
+
+def main():
+    file = dir(hidden_4)
+    length = len(file)
+    for i in range(0, length):
+        if file[i][0:2] != "__":
+            print(file[i])
+
+
+if __name__ == "__main__":
+    main()
