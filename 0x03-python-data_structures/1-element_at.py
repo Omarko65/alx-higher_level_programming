bc00717d51e26559947d0b21b5b7def598c1a2bb
holic65 <okormark@gmail.com>
2022-06-06 14:25:13 -0700

    task 1 fixed

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,8 +2,8 @@
 def element_at(my_list, idx):
     lent = len(my_list)
     if idx < 0:
-        return "None"
+        return (None)
     elif idx > lent:
-        return "None"
+        return (None)
     else:
-        return my_list[idx]
+        return (my_list[idx])
