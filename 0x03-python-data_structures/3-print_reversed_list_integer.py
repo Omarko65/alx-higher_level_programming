1d12ff7154164539ecd8511e68f5f5552ceae2eb
holic65 <okormark@gmail.com>
2022-06-06 15:01:53 -0700

    Task 3 push

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    lent = len(my_list) - 1
+    while 0 <= lent:
+        print("{:d}".format(my_list[lent]))
+        lent = lent - 1
