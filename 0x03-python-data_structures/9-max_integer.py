eb583d3238cf8f16672d73ebd9fb50aa76821699
holic65 <okormark@gmail.com>
2022-06-07 05:46:44 -0700

    Task 9 fixed

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    highest_num = my_list[0]
     if len(my_list) < 1:
         return None
     else:
+        highest_num = my_list[0]
         for i in my_list:
             if i > highest_num:
                 highest_num = i
--- 0x03-python-data_structures/main.py
+++ 0x03-python-data_structures/main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-delete_at = __import__('11-delete_at').delete_at
+max_integer = __import__('9-max_integer').max_integer
 
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_list = delete_at(my_list, idx)
-print(new_list)
-print(my_list)
+my_list = [-1, -2, -3, -4]
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
