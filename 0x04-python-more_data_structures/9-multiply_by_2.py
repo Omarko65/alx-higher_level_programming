73634b12533ec0159af98af0e2ece95c0cc224dc
holic65 <okormark@gmail.com>
2022-06-08 19:32:42 -0700

    Task 9 fixed

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    c = a_dictionary.copy()
-    for i in c:
-        c[i] *= 2 
-    return c
+    new_d = a_dictionary.copy()
+    for i in new_d:
+        new_d[i] *= 2 
+    return new_d
