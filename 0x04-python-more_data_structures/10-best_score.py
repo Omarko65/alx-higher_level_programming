a27b89a609094f0ecd621c25c468ca6405fd6892
holic65 <okormark@gmail.com>
2022-06-08 19:49:50 -0700

    task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    max_score = 0
+    best = ""
+    for key, value in a_dictionary.items():
+        if value > max_score:
+            max_score = value
+            best = key
+    return best
+    
