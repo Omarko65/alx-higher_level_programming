3eea2f68a533e815b49d1a4d0d49b486f337952f
holic65 <okormark@gmail.com>
2022-06-08 18:44:56 -0700

    Task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if key != a_dictionary:
+        a_dictionary[key] = value
+    else:
+        del a_dictionary[key]
+        a_dictionary[key] = value
+    return a_dictionary
