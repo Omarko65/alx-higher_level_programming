ca6361251980b615f6578613900bcef95e55d371
holic65 <okormark@gmail.com>
2022-06-14 06:09:27 -0700

    Task

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for ch in reversed(range(97, 123)):
-    print("{:c}".format(ch if (ch % 2 == 0) else (ch - 32)), end='')
+for a in range(122, 96, -1):
+    if a % 2 != 0:
+        print("{:c}".format(a - 32), end='')
+    else:
+        print("{:c}".format(a), end='')
