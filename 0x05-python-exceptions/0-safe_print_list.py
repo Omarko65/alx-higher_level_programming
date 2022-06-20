f74d31f92c208668d1c4a558c8f1d4d0ef98be8a
holic65 <okormark@gmail.com>
2022-06-20 16:47:19 -0700

    TASK 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    mass = 0
+    c = ""
+    ret_list = list(my_list)
+    for j in my_list:
+        mass+=1
+    try:
+        if x<= mass:
+            print(''.join(str(i) for i in ret_list[:x]))
+        elif x > mass:
+            print(''.join(str(i) for i in ret_list[:mass]))
+    except TypeError:
+        print("Invalid type")
+    else:
+        if x <= mass:
+            return (x)
+        else:
+            return (mass)
