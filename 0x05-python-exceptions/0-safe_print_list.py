13b80c1706a2836ac95d81371684dd4b9e53d9bb
holic65 <okormark@gmail.com>
2022-06-20 16:51:23 -0700

    TASK 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     c = ""
     ret_list = list(my_list)
     for j in my_list:
-        mass+=1
+        mass += 1
     try:
         if x <= mass:
             print(''.join(str(i) for i in ret_list[:x]))
