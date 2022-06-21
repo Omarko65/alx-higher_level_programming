e270479232db81e1df1a444c1902e3c1feb01535
holic65 <okormark@gmail.com>
2022-06-21 02:27:05 -0700

    fixed

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,9 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except:
+    except ValueError:
+        return (False)
+    except Exceptation:
         return (False)
     else:
         return (True)
