56c1bf0615f0d5b9e82d3d74efff2d22c47e9774
holic65 <okormark@gmail.com>
2022-06-20 17:30:23 -0700

    Task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,7 +4,7 @@ def safe_print_integer(value):
         print("{:d}".format(value))
     
     except:
-        return False
+        return (False)
     
     else:
-        return True
+        return (True)
