7da9e8c26b821529537a15146d19cbef92e2c0eb
holic65 <okormark@gmail.com>
2022-06-22 15:42:38 -0700

    task 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -30,6 +30,7 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
+        
         else:
             for i in range(self.__size):
                 for j in range(self.__size):
