cb5b6b2639fb8b2fb9abf222b27eba57d5ca63a8
holic65 <okormark@gmail.com>
2022-06-22 13:59:20 -0700

    Task

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -17,7 +17,7 @@ class Square:
     def __init__(self, size):
         if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
