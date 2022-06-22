f9fd4f398895ee31fffc6cb0d359ef6a43c0e7bd
holic65 <okormark@gmail.com>
2022-06-22 15:11:10 -0700

    task

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+Module 4-square
+class square module which has different classes
+'''
+
+
+class Square:
+    '''class square is initialized here'''
+    def __init__(self, size=0):
+        self.size = size
+    
+    
+
+    @property
+    def size(self):
+        return self.__size
+    
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+    
+    def area(self):
+        return self.__size ** 2
+
