baf6b4ce35530507a71784dcb5d9785af46a4a58
holic65 <okormark@gmail.com>
2022-06-22 08:38:50 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+Define class Square with private attribute
+"""
+class Square:
+    '''
+    Class square with private size attribute
+    Args:
+        __size: private size attribute
+    '''
+    def __init__(self, __size):
+        self.__size = __size
