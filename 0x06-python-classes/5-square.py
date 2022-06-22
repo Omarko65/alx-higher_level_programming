9141032b0538c835c8ba4ff798ac77d422d2f6b9
holic65 <okormark@gmail.com>
2022-06-22 15:33:38 -0700

    fixed

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -27,7 +27,10 @@ class Square:
     def area(self):
         return self.__size * self.__size
     def my_print(self):
-        for i in range(self.__size):
-            for j in range(self.__size):
-                print("#", end = "")
-            print("\n")
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end = "")
+                print("\n")
