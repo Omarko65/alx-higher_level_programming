76ad0beb306be3b4d278c7d94e7177054e7a9b57
holic65 <okormark@gmail.com>
2022-06-22 15:13:21 -0700

    task

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -9,13 +9,11 @@ class Square:
     '''class square is initialized here'''
     def __init__(self, size=0):
         self.size = size
-    
-    
 
     @property
     def size(self):
         return self.__size
-    
+
 
     @size.setter
     def size(self, value):
@@ -29,4 +27,3 @@ class Square:
     
     def area(self):
         return self.__size ** 2
-
