e20ae1cf9ed09051e514e29c4fc6bcf33b1630ba
holic65 <okormark@gmail.com>
2022-06-22 08:46:54 -0700

    Task 1 fixed

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,11 +5,11 @@ Define class Square with private attribute
 
 """
 class Square:
-    '''
-    Class square with private size attribute
-    Args:
-        __size: private size attribute
-    '''
+'''
+Class square with private size attribute
+Args:
+    __size: private size attribute
+'''
 
     def __init__(self, __size):
         self.__size = __size
