e2bc61f1c549f1caa020b94ce93e73f3eaa22210
holic65 <okormark@gmail.com>
2022-06-22 10:19:14 -0700

    push

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -15,4 +15,4 @@ class Square:
             else:
                 raise TypeError("size must be >= 0")
         else:
-            raise TypeError("sise must be integer")
+            raise TypeError("sise must be an integer")
