e8ad6e55a63941a2427ad623fedc91551be63105
holic65 <okormark@gmail.com>
2022-06-27 15:06:34 -0700

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+'''define class rectangle module'''
+
+
+class Rectangle:
+    '''class rectangle initialized'''
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    '''private attribute getter for width'''
+    @property
+    def width(self):
+        return self.__width
+
+    '''private attribute setter for width'''
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise TypeError("width must be >= 0")
+        else:
+            self.__width = value
+
+    '''private attribute getter for width'''
+    @property
+    def height(self):
+        return self.__height
+
+    '''private attribute setter for width'''
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise TypeError("height must be >= 0")
+        else:
+            self.__height = value
