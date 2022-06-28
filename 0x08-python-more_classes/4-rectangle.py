2bfc6069b30b86665f0d3bdddde3e654b4e8819d
holic65 <okormark@gmail.com>
2022-06-28 05:11:49 -0700

    task 4 fixing minors

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -45,7 +45,6 @@ class Rectangle:
         count = 1
         if self.__height == 0 and self.__width == 0:
             return myprint
-
         else:
             for i in range(self.__height):
                 for j in range(self.__width):
