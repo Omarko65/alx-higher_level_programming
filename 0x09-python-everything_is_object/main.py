a80d576e9e48d074e183fd72a0334200ad6e0dc6
holic65 <okormark@gmail.com>
2022-06-29 16:25:10 -0700

    task

--- 0x09-python-everything_is_object/main.py
+++ 0x09-python-everything_is_object/main.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-copy_list = __import__('19-copy_list').copy_list
+LockedClass = __import__('101-locked_class').LockedClass
 
-my_list = [1, 2, 3]
-print(my_list)
+lc = LockedClass()
+lc.first_name = "John"
+try:
+    lc.last_name = "Snow"
+except Exception as e:
+    print("[{}] {}".format(e.__class__.__name__, e))
 
-new_list = copy_list(my_list)
-
-print(my_list)
-print(new_list)
-
-print(new_list == my_list)
-print(new_list is my_list)
