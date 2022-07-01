6821a044fc631869af369981e046f55849ccd0f5
holic65 <okormark@gmail.com>
2022-07-01 15:25:47 -0700

    task 3

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def say_my_name(first_name="", last_name=""):
+    if type(first_name) not in [str]:
+        raise TypeError("first_name must be a string")
+
+    if type(last_name) not in [str]:
+        raise TypeError("last_name must be string")
+
+    else:
+        print("My name is {} {}".format(first_name, last_name))
