173fe80856ca915b76203d3783b6e2486cab36fe
holic65 <okormark@gmail.com>
2022-07-05 16:32:10 -0700

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''read_file module definition'''
+
+
+def read_file(filename=""):
+    '''read_file function definition'''
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+    print(read_data)
