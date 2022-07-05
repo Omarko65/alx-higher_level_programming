41361e13544dbc26f728d792af100bcdc9a288da
holic65 <okormark@gmail.com>
2022-07-05 16:33:58 -0700

    task 6

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     '''read_file function definition'''
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
-    print(read_data)
+        print(read_data)
