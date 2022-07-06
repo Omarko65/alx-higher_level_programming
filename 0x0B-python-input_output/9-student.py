394d762160ad1f7048a239a172b7f0f5f14f9030
holic65 <okormark@gmail.com>
2022-07-06 01:59:29 -0700

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -4,11 +4,13 @@
 
 class Student:
     '''class student initialization'''
+
     def __init__(self, first_name,last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     '''definition of method that retrieves a dict'''
+
     def to_json(self):
         return self.__dict__
