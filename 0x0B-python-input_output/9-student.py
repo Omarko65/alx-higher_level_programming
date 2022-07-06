531fcee62c6de02bbc37f3e1358a0f0160c67491
holic65 <okormark@gmail.com>
2022-07-06 01:57:47 -0700

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''defining student class'''
+
+
+class Student:
+    '''class student initialization'''
+    def __init__(self, first_name,last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    '''definition of method that retrieves a dict'''
+    def to_json(self):
+        return self.__dict__
