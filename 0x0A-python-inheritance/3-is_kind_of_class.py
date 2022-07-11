a7e34a8c46418c5e15165e53bffefa1d08256974
holic65 <okormark@gmail.com>
2022-07-11 14:40:59 -0700

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''is_kind_of_class module definition'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''a function that returns true if obj an instance is same as a_class'''
+    if obj is None or a_class is None or isinstance(obj, a_class) is not True:
+        return False
+    if isinstance(obj, a_class) is True:
+        return True
