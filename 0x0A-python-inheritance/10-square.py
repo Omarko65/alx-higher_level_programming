4e8b902c949ddf82eb21109b974e3f839a028cbe
holic65 <okormark@gmail.com>
2022-07-16 07:54:14 -0700

    task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,9 +9,9 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     '''class initialization'''
     def __init__(self, size):
-        super().__init__(size, size)
         self.__size = size
         self.integer_validator("size", self.__size)
+        super().__init__(size, size)
         
 
     '''area implementation'''
