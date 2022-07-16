e640b58933b51c0858cdb82e9b2e33f5471d5ae6
holic65 <okormark@gmail.com>
2022-07-16 06:27:47 -0700

    task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''module definition of rectangle class definition'''
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+'''importing parent directory to be used'''
+
+class Rectangle(BaseGeometry):
+    '''Rectangle class defined'''
+
+    def __init__(self, width, height):
+        '''rectangle class initialization'''
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
