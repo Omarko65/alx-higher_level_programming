c83245c7bb39eb06de3f29116853e763626b9dfd
holic65 <okormark@gmail.com>
2022-07-16 08:01:56 -0700

    task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''module for square class'''
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+'''rectangle class import'''
+
+
+class Square(Rectangle):
+    '''class initialization'''
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", self.__size)
+        super().__init__(size, size)
+
+    '''area implementation'''
+    def area(self):
+        return self.__size ** 2
+
+    '''defining str'''
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
