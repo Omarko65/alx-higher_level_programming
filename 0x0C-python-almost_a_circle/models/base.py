0555d6546da951c2fbc9079f30c95994a01343ec
holic65 <okormark@gmail.com>
2022-08-06 15:28:29 -0700

    task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,6 +16,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         '''method that returns json repr of dict'''
         if list_dictionaries is None:
