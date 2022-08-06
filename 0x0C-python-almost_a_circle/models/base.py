f44cdbb2d5e945762a5d327432dbdc8d640b805c
holic65 <okormark@gmail.com>
2022-08-06 16:13:27 -0700

    task

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,3 +33,11 @@ class Base:
             else:
                 list_dicts = [obj.to_dictionary() for obj in list_objs]
                 f.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''static method that returns list of json repr'''
+        if json_string is None:
+            return []
+        else:
+            return json.loads(json_string)
