bb4fc19cfdfb88be10a3220f7e0c7896f6efc20d
holic65 <okormark@gmail.com>
2022-08-06 14:37:17 -0700

    task

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,3 +25,13 @@ class Square(Rectangle):
         '''str initialization of class'''
         return "[{}] ({}) {}/{} - {}".format(
                 type(self).__name__, self.id, self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        '''adding args and kwargs function to class'''
+        attrs = ('id', 'size', 'x', 'y')
+        for key, val in zip(attrs, args):
+            setattr(self, key, val)
+        if (type(args) is None or len(args) == 0) and (type(kwargs) is dict):
+            for key, val in kwargs.items():
+                if key in attrs:
+                    setattr(self, key, val)
