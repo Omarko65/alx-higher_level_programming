6d61ddc37700ab818619c0e18cf4f83354d5daff
holic65 <okormark@gmail.com>
2022-09-06 16:52:48 -0700

    task'

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 // A script that prints a message depending on Args
-const command = process.argv[2];
 if (process.argv[3]) {
   console.log('Arguments found');
 } else if (process.argv[2]) {
