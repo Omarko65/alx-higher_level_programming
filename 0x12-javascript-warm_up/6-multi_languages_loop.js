3d9e38881be30e1459fbbbf5a5c0db9b22cc51e1
holic65 <okormark@gmail.com>
2022-09-07 16:30:31 -0700

    task

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// A script that prints 3 line of string using loop
+const language = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let i = 0;
+while (language[i]) {
+  console.log(language[i]);
+  i++;
+}
