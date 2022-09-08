e6b0e6186e4c48a55d77e20454f86c9ba864bfc9
holic65 <okormark@gmail.com>
2022-09-07 19:21:12 -0700

    task

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  list.forEach(function (item) {
+    if (item === searchElement) {
+      count++;
+    }
+  });
+  return count;
+};
