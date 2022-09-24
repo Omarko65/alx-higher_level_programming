7c6f7926aba02d1aa78befe7d9d84514dc6311ff
Holic65 <okormark@gmail.com>
2022-09-23 17:31:07 -0700

    task

--- 0x0C-python-almost_a_circle/tests/print_function.py
+++
@@ -1,8 +0,0 @@
-from __future__ import print_function
-import unittest
-class TestFoo(unittest.TestCase):
-     def test_foo(self):
-             self.assertTrue(True)
-     def fun_not_run(self):
-             print("no run")
-unittest.main()
