0ad45e6ae2c01783ad0798e0732f9535b27546b2
holic65 <okormark@gmail.com>
2022-11-10 19:58:54 -0800

    taks 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''a script that fetches head value X-Request-id from url'''
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    e_address = sys.argv[2]
+    mail = {'email': e_address}
+    r = requests.post(url, data= mail)
