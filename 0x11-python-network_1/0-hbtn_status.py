9746d36dc7d9d05abb8685746b783a10136a7731
holic65 <okormark@gmail.com>
2022-11-10 17:33:27 -0800

    fixed error 8

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(req) as response:
         the_page = response.read()
         print('Body response:')
-        print("\t- type: <class 'bytes'>")
-        print("\t- content: b'OK'")
-        print("\t- utf8 content: OK")
+        print("\t- type: {}".format(type(the_page)))
+        print("\t- content: {}".format(the_page)))
+        print("\t- utf8 content: {}".format(page.decode("utf-8")))
