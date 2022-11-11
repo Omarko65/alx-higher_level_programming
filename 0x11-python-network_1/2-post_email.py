2a7b170481d9dd6de4620f1781526884e4239a74
holic65 <okormark@gmail.com>
2022-11-10 18:39:44 -0800

    fixing error

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1]
     e_address = sys.argv[2]
-    data = urllib.parse.urlencode({'email': e_address}))
+    data = urllib.parse.urlencode({'email': e_address})
     data = data.encode('utf-8')
     with urlopen(url, data) as response:
         body = response.read()
