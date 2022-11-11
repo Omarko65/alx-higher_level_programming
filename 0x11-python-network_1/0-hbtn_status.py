ce3c5de2a9a1b96b2c974a5fed1b66dd16fcbdaf
holic65 <okormark@gmail.com>
2022-11-10 17:34:43 -0800

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
         the_page = response.read()
         print('Body response:')
         print("\t- type: {}".format(type(the_page)))
-        print("\t- content: {}".format(the_page)))
-        print("\t- utf8 content: {}".format(page.decode("utf-8")))
+        print("\t- content: {}".format(the_page))
+        print("\t- utf8 content: {}".format(the_page.decode("utf-8")))
