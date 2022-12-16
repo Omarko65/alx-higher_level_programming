f44c8625da24c190fbd9ac39d176f17a8c52ce7f
holic65 <okormark@gmail.com>
2022-12-15 18:27:46 -0800

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+''' a module that finds the peak of list of unsorted ints '''
+
+
+def find_peak(list_of_integers):
+    '''module definition of find_peak'''
+    if len(list_of_integers) == []:
+        return None
+
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    elif len(list_of_integers) == 2:
+        return max(list_of_integers)
+
+    
+    mid = int(len(list_of_integers) / 2)
+    peak = list_of_integers[mid]
+
+    if peak > list_of_integers[mid + 1] and peak > list_of_integers[mid - 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
