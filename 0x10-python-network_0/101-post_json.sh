338ee527f5b44c03d40a6f8e252027ec91563732
holic65 <okormark@gmail.com>
2022-12-15 19:17:02 -0800

    push errors

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that post json file to url with curl
-curl -sX POST "Content-Type: application/json" -d "$(cat "$2")" "$1"
+curl -sX POST -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a script that causes server to respond with message
-curl -sX PUT -L -d "user-id=98" -H "origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+curl -sX PUT -L -d "user_id=98" -H "origin: HolbertonSchool" 0.0.0.0:5000/catch_me
