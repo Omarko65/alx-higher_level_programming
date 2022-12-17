5924cc350ac2f68d204393d7b78a73ba4fc4f4a4
holic65 <okormark@gmail.com>
2022-12-16 17:38:18 -0800

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+''' a script that fetches url using urllib'''
+import urllib.request
+
+
+url = 'https://alx-intranet.hbtn.io/status'
+req = urllib.request.Request(url)
+with urllib.request.urlopen(req) as response:
+    html = response.read()
+
+print('Body reponse:')
+print('\t- type: {}'.format(type(html)))
+print('\t- content: {}'.format(html))
+print('\t- utf8 content: {}'.format(html.decode('utf-8')))
