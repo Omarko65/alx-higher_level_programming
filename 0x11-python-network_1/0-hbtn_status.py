606ec6d623463714ff010ab28bc86fda6678484a
holic65 <okormark@gmail.com>
2022-12-16 17:54:51 -0800

    0-hbtn

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-''' a script that fetches url using urllib'''
+''' a script that fetches alx url '''
 import urllib.request
 
 
-if __name__ == '__main__':
-    '''script definition'''
-    url = 'https://alx-intranet.hbtn.io/status'
+if __name__ == "__main__":
+    ''' module to fetch url '''
+    url = 'http://alx-intranet.hbtn.io/status'
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
-        html = response.read()
-        print('Body reponse:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode('utf-8')))
+        the_page = response.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(the_page)))
+        print("\t- content: {}".format(the_page))
+        print("\t- utf8 content: {}".format(the_page.decode("utf-8")))
