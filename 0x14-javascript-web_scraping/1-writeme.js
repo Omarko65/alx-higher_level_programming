6e33322461bc920eb65c4681262dd76032b993fc
holic65 <okormark@gmail.com>
2023-01-22 17:19:31 -0800

    task1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// a script that writes to a file
+
+const filepath = process.argv[2];
+const content = process.argv[3];
+const fs = require('fs');
+
+if (filepath && content) {
+  fs.writeFile(filepath, content, 'utf8', (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log('');
+  });
+}
