4963904be55451f066a6c666e57bdbe2e5f749ff
holic65 <okormark@gmail.com>
2023-01-22 17:34:06 -0800

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,12 +5,8 @@ const filepath = process.argv[2];
 const content = process.argv[3];
 const fs = require('fs');
 
-if (filepath && content) {
-  fs.writeFile(filepath, content, 'utf8', (err) => {
-    if (err) {
-      console.error(err);
-    }
-  });
-} else {
-  console.log('No file path passed');
-}
+fs.writeFile(filepath, content, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
